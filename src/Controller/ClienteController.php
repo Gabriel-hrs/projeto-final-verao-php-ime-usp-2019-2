f4c1b7e5ed79fe1e91eb80a5f485d56b037e6219
Gabriel-hrs <44143105+Gabriel-hrs@users.noreply.github.com>
2019-02-17 21:22:11 -0300

    Cadastro de usuário funcionando

--- src/Controller/ClienteController.php
+++ src/Controller/ClienteController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\Cliente;
+use App\Repository\Banco;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -26,6 +27,8 @@ class ClienteController extends AbstractController {
 		$cliente->setSenha($senha);
 		
 		$erros = array();
+		$sucessos = array();
+
 		if ($_POST) {
 			if (!$nome) {
 				$erros[] = 'Digite o nome!';
@@ -47,6 +50,51 @@ class ClienteController extends AbstractController {
 				$senha = md5($cliente->getSenha());
 				$strsql = "insert into clientes (nome, email, telefone, senha) values ('" . $cliente->getNome() . "', '" . $cliente->getEmail() . "', '', '$senha')";
 				echo $strsql;
+
+				
+				$host = 'localhost';
+				$user = 'root';
+				$pass = '';
+				$base = 'verao-2019';
+				$conn = new \mysqli($host, $user, $pass, $base);
+				if (\mysqli_connect_errno()) {
+					exit("Não foi possível conectar no banco de dados!");
+				}			
+
+				//Verifica se houve erro de conexão:
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				}
+				//teste
+				$conn->query($strsql);
+				//teste
+
+
+				/*if ($conn->query($sql) === TRUE) {
+					echo "<br>Cadastro efetuado com sucesso.<br>";
+				} else {
+					echo "Error: " . $sql . "<br>" . $conn->error;
+				}
+				*/
+
+				//Encerra a conexão com o servidor:
+				$conn->close();
+
+				//$insert = mysql_query($strsql, $banco);
+
+				//$cliente = new Cliente();
+				//$id = $cliente->getId();
+				//echo $id;
+
+				/*if ($strsql->execute()) {
+					$sucessos[] = 'Cliente cadastrado com sucesso!';
+					//header('Location: login.php');
+				}*/
+
+				/*if ($insert) {
+					echo "Sucesso! Row ID: {$banco->insert_id}";
+					$sucessos[] = 'Cliente cadastrado com sucesso!';
+				}*/
 			}	
 		}
 		
@@ -56,8 +104,8 @@ class ClienteController extends AbstractController {
 		return $this->render('cliente/form.html.twig', [
 			'cliente' => $cliente,
 			'senhaConfirmacao' => $senhaConfirmacao,
-			'erros' => $erros
-			
+			'erros' => $erros,
+			'sucessos' => $sucessos			
 		]);
 	}
 
--- src/Repository/Banco.php
+++ src/Repository/Banco.php
@@ -10,7 +10,7 @@ class Banco {
 		$user = 'root';
 		$pass = '';
 		$base = 'verao-2019';
-		$banco = new \mysqli($host, $user, $pass, $base);
+		$banco = new mysqli($host, $user, $pass, $base);
 		if (\mysqli_connect_errno()) {
 			exit("Não foi possível conectar no banco de dados!");
 		}
